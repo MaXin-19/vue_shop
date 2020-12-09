756c37647cf91c262fd79616c6bc17ed7f7daa9c
MaXin-19 <mcxxfl@yeah.net>
2020-12-09 17:00:20 +0800

    1209_2登录退出界面（164视频）

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>登录组件</div>
+  <div class='login_container'>
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png">
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="primary">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +30,57 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+
+.login_container{
+  background-color:#2b4b6b;
+  height:100%;
+}
+
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius:3px;
+  position:absolute;
+  left:50%;
+  top:50%;
+  margin-left: -225px;
+  margin-top:-150px;
+  // transform:translate(-50%,-50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border:1px solid #eee;
+    border-radius:50%;
+    padding:10px;
+    box-shadow:0 0 10px #ddd;
+    position:absolute;
+    left:50%;
+    // top:50%;
+    transform: translate(-50%,-50%);
+    // top:65px;
+    background-color: rgb(34, 22, 19);
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius:50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form{
+  position:absolute;
+  bottom:0px;
+  width: 100%;
+  padding: 0 40px;
+  box-sizing: border-box;
+}
+
+.btns{
+  display:flex;
+  justify-content: flex-end;
+}
+
 </style>
